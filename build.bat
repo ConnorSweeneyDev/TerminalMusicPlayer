d38e3beaa4c6a807bfda63f38d47687aa5cfaed3
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-30 02:53:39 +0100

    Release configuration

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
 IF NOT EXIST bin ( MKDIR bin)
-start pwsh -NoExit -Command "g++ -g -Wall -Wextra -Wpedantic -Iprog/include prog/source/*.cpp -lwinmm -o bin/TerminalMusicPlayer.exe"
+start pwsh -NoExit -Command "g++ -s -O3 -std=c++20 -DNDEBUG -Iprog/include prog/source/*.cpp -lwinmm -o bin/TerminalMusicPlayer.exe"
