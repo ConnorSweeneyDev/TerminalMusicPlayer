e8f179c2e6a47336a076282567a74b67b4b7eea6
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-10-11 23:49:29 +0100

    Fixed debug bug

--- script/debug.bat
+++ script/debug.bat
@@ -2,4 +2,4 @@
 
 SET COMMAND=pwsh -Command "gdb -tui ./binary/TerminalMusicPlayer.exe"
 IF "%1" == "-wezterm" (wezterm cli spawn --cwd %CD% %COMMAND%)
-IF "%1" == "" (START %COMMAND%)
+IF "%1" == "" (%COMMAND%)
